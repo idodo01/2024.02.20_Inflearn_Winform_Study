323a88c211ddfa89c690c129b5693a2fd5eb62e5
BYUNSUJUNG <susususu07@naver.com>
2025-02-24 00:02:17 +0900

    git init 2025-02-20

---
+++ Models/WinformDbContext.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WinFormsAppMaster.Models;
+
+public partial class WinformDbContext : DbContext
+{
+    public WinformDbContext()
+    {
+    }
+
+    public WinformDbContext(DbContextOptions<WinformDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<GangnamguPopulation> GangnamguPopulations { get; set; }
+
+    public virtual DbSet<Test> Tests { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=winform_db;Username=postgres;Password=dntkrlgkxm1!");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("pg_catalog", "adminpack");
+
+        modelBuilder.Entity<GangnamguPopulation>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("gangnamgu_population_pkey");
+
+            entity.ToTable("gangnamgu_population");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.AdministrativeAgency)
+                .HasColumnType("character varying")
+                .HasColumnName("administrative_agency");
+            entity.Property(e => e.FemalePopulation).HasColumnName("female_population");
+            entity.Property(e => e.MalePopulation).HasColumnName("male_population");
+            entity.Property(e => e.NumberOfHouseholds).HasColumnName("number_of_households");
+            entity.Property(e => e.NumberOfPeoplePerHousehold).HasColumnName("number_of_people_per_household");
+            entity.Property(e => e.SexRatio).HasColumnName("sex_ratio");
+            entity.Property(e => e.TotalPopulation).HasColumnName("total_population");
+        });
+
+        modelBuilder.Entity<Test>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("test_pkey");
+
+            entity.ToTable("test");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Name)
+                .HasColumnType("character varying")
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
